9115d12e55025e00399b1f537c224645f0f5c38c
TheSteve0 <steve.pousty@gmail.com>
2023-05-21 13:21:02 -0700

    Initial Upload and finished

---
+++ src/main/java/com/molw/bluespring/BluespringApplication.java
@@ -0,0 +1,13 @@
+package com.molw.bluespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluespringApplication.class, args);
+    }
+
+}
